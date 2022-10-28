f75d0c0c876bc634a311169036a4100f5555141f
monteirolc <lucasm.castro47@gmail.com>
2022-10-28 19:59:31 -0300

    Create HTML for pages

--- projeto/urls.py
+++ projeto/urls.py
@@ -14,17 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.http import HttpResponse
-from django.urls import path
-
-
-# HTTP REQUEST
-def my_view(request):
-    return HttpResponse('Uma linda string')
-    # HTTP RESPONSE
-
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', my_view)
+    path('', include('recipes.urls')),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'recipes/home.html', context={'name': 'Lucas'})
+
+
+def contato(reuqest):
+    return HttpResponse('Contato')
+
+
+def sobre(request):
+    a = 2
+    a = a + 2
+    return HttpResponse(f'sobre + {a}')
