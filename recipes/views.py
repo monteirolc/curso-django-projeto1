79d99863fbad57fa07c1e6696374d9b7eb22f9d6
monteirolc <lucasm.castro47@gmail.com>
2022-11-01 16:25:47 -0300

    Create views and same partials

--- recipes/views.py
+++ recipes/views.py
@@ -1,18 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={'name': 'Lucas'})
-
-
-def contato(reuqest):
-    return HttpResponse('Contato')
-
-
-def sobre(request):
-    a = 2
-    a = a + 2
-    return HttpResponse(f'sobre + {a}')
+    return render(request, 'recipes/pages/home.html', context={'name': 'Lucas'})
