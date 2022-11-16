fe6f9e3fb921a536527f64af3b0aba02c023fe58
castrolm <lucasm.castro47@gmail.com>
2022-11-15 21:03:50 -0300

    fix problems with recipe, create html and category views

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from recipes import views
+from . import views
 
 app_name = 'recipes'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('recipes/category/<int:category_id>', views.category,
+         name='category'),
     path('recipes/<int:id>/', views.recipe, name='recipe'),
-
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,18 +1,43 @@
-from django.shortcuts import render
+# from django.http import Http404
+from django.shortcuts import get_list_or_404, render
 
-from utils.recipes.factory import make_recipe
+from .models import Recipe
+
+# from utils.recipes.factory import make_recipe
+
 
 # Create your views here.
 
 
 def home(request):
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
+    })
+
+
+def category(request, category_id):
+    # recipes = Recipe.objects.filter(
+    #     category__id=category_id, is_published=True,
+    # ) .order_by('-id')
+    # if not recipes:
+    #     raise Http404('Not Found ')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id, is_published=True,).order_by('-id')
+    )
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category | '
+        # 'title': f'{recipes.first().category.name} - Category | '
     })
 
 
 def recipe(request, id):
+    recipe = Recipe.objects.filter(
+        pk=id, is_published=True,).order_by('-id').first()
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipes': make_recipe(),
+        'recipe': recipe,
+        'title': f'{recipe.title}',
         'is_detail_page': True,
     })
