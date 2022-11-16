88b5fdf600598f4652df89f5b62497f31e2a7dd2
castrolm <lucasm.castro47@gmail.com>
2022-11-15 21:10:49 -0300

    Changed to get_object_or_404

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
 # from django.http import Http404
-from django.shortcuts import get_list_or_404, render
+from django.shortcuts import get_list_or_404, render, get_object_or_404
 
 from .models import Recipe
 
@@ -34,8 +34,7 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(
-        pk=id, is_published=True,).order_by('-id').first()
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'title': f'{recipe.title}',
