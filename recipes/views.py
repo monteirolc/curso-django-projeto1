823f1a2bed523d354e1ebc157a33a7081fcd6934
castrolm <lucasm.castro47@gmail.com>
2022-11-15 21:37:04 -0300

    changed home to list_or_404

--- recipes/views.py
+++ recipes/views.py
@@ -10,7 +10,9 @@ from .models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(is_published=True,).order_by('-id')
+    )
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
