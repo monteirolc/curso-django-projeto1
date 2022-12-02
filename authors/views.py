258425719c0a3587809e45f15ee0a05f7732fa7c
castrolm <lucasm.castro47@gmail.com>
2022-12-02 19:34:05 -0300

    changing view to create and register, replace urls

--- authors/views.py
+++ authors/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import redirect, render
+
+from .forms import RegisterForm
 
 
-# Create your views here.
 def register_view(request):
-    return render(request, 'authors/pages/register_view.html')
+    register_form_data = request.session.get('register_form_data', None)
+    form = RegisterForm(register_form_data)
+    return render(request, 'authors/pages/register_view.html', context={
+        'form': form,
+    })
+
+
+def register_create(request):
+    if not request.POST:
+        raise Http404()
+
+    POST = request.POST
+    request.session['register_form_data'] = POST
+    form = RegisterForm(POST)
+
+    return redirect('authors:register')
--- project/urls.py
+++ project/urls.py
@@ -19,9 +19,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('recipes.urls')),
     path('authors/', include('authors.urls')),
-
+    path('', include('recipes.urls')),
+    path('admin/', admin.site.urls),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
