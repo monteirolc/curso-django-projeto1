6ff3dcc3cadbdf23122686cc9d6320dd6710666c
castrolm <lucasm.castro47@gmail.com>
2023-01-03 18:18:00 -0300

    Create model Tag

--- project/settings.py
+++ project/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -150,3 +151,9 @@ MESSAGE_TAGS = {
     constants.SUCCESS: 'message-success',
     constants.WARNING: 'message-warning',
 }
+
+# Debug toolbar
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
